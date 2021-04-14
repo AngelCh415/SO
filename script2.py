553df1796a663f89272cb873a735626413d06bd1
AngelCh415 <angelchavezmorones@gmail.com>
2021-04-14 18:09:54 -0500

    Falta corroborar que este activo, inicio de empaqutar

--- script2.py
+++ script2.py
@@ -11,45 +11,49 @@ def ip_valida(ip):
         return False
 def ip_activa(ip):
     if(ip_valida(ip)== True):
-        
         p = subprocess.Popen(['ping', '-n', '2', '-w', '2', ip])
         p.wait()
-        return 1 if p.poll()==0 else 0
+        return 1 if p.poll()==0 else 1
     else:
         return 0
+def obtenerClaseIP(ip):
+	direccionip = ip.split('.')
+	primerOcteto = int(direccionip[0])
+	if(primerOcteto >= 1 and primerOcteto <=127):
+		return 'A'
+	elif(primerOcteto >=128 and primerOcteto <=191):
+		return 'B'
+	elif(primerOcteto >= 192 and primerOcteto <= 223):
+		return 'C'
+	else:
+		return 'Unknown'
+def aproximarMascara(claseIP):
+	if (claseIP == 'A'):
+		return '255.0.0.0'
+	elif(claseIP == 'B'):
+		return '255.255.0.0'
+	elif(claseIP=='C'):
+		return '255.255.255.0'
+	else:
+		return 'Unknown'
+ip_func = []
+masc_func = []
+i=0
+for iface in netifaces.interfaces():
+    if iface == 'lo' or iface.startswith('vbox'):
+        continue
+    iface_details = netifaces.ifaddresses(iface)
+    if iface_details.__contains__(netifaces.AF_INET):
+        print ("Tus red es: ", iface_details[netifaces.AF_INET])
+        for ip_interface in iface_details[netifaces.AF_INET]:
+            for llave, direccion_ip in ip_interface.items():
+                if llave== 'addr' and direccion_ip != '127.0.0.1': 
+                    if(ip_activa(direccion_ip)):
+                        print("IP activa", direccion_ip)
+                        ip_func+= str(direccion_ip)
+                        masc_func += aproximarMascara(obtenerClaseIP(direccion_ip))
 
-interfaces = netifaces.interfaces()
 
-print ("Las interfaces son: ", interfaces)
-dirrecciones_activas = []
-
-for interface in interfaces:
-#Se captura la informacion de cada interfaz
-    datos = netifaces.ifaddresses(interface)
-print ("————————–")
-#Se muestra el nombre de la interface
-print ("Interface: %s", interface)
-#Se captura la lista de parametros que tiene la interface
-variables = datos.keys()
-#Se muestra la direccion de la capa de enlace de red de la interface
-print ("Capa de enlace de red: ", datos[netifaces.AF_LINK][0]['addr'])
-#Si esta presente la informacion de IPV4 se muestra
-if netifaces.AF_INET in variables:
-    print ("IPv4  IP: ",datos[netifaces.AF_INET][0]['addr'],"Mascara:", datos[netifaces.AF_INET][0]['netmask'])
-    if(ip_activa(datos[netifaces.AF_INET][0]['addr'])):
-        print("IP Activa")
-        dirrecciones_activas += datos[netifaces.AF_INET][0]['addr']
-    else:
-        print("IP no Activa")
-#Si esta presente la informacion de IPv6 se muestra
-if netifaces.AF_INET in variables:
-    print ("IPv6: IP: ",datos[netifaces.AF_INET6][0]['addr'],"Mascara:", datos[netifaces.AF_INET6][0]['netmask'])
-    if(ip_activa(datos[netifaces.AF_INET6][0]['addr'])):
-        print("Ip Activa")
-        dirrecciones_activas += datos[netifaces.AF_INET6][0]['addr']
-    else:
-        print("IP no Activa")
-    
 
 
 
